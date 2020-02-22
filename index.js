d14efa6d0c8bde55587b579a0509793d79999df6
mohamed Ibrahim <muhmds@hotmail.com>
2020-02-22 02:04:30 -0500

    2nd push

--- index.js
+++ index.js
@@ -14,10 +14,47 @@ const questions = [
     type: "list",
     message: "Select your favorite color:",
     name: "color",
-    choices: ["green", "blue", "pink", "red"]
+    choices: ["green", "blue", "grey", "black"]
   }
 ];
 
 const askQuestions = () => {
     return inquirer.prompt(questions);
   };
+
+  const getGitResponse = data => {
+    const queryUrl = `https://api.github.com/users/${data.username}`;
+    const starredUrl = `https://api.github.com/users/${data.username}/starred`;
+    return axios.all([axios.get(queryUrl), axios.get(starredUrl)]);
+  };
+
+  const readFromFile = page => {
+    fs.readFile(`${page}`, (err, data) => {
+      if (err) console.log(chalk.inverse.yellow(`Something went wrong: ${err}`));
+      return data;
+    });
+  };
+
+  const convertToPDF = page => {
+    const options = {
+      format: "Legal"
+    };
+    pdf.create(page, options).toFile("./profile.pdf", function(err, res) {
+      if (err) return console.log(chalk.yellow(`Something went wrong ${err}`));
+      console.log(chalk.green(`Profile PDF written`));
+    });
+  };
+
+  async function init() {
+    try {
+      const data = await askQuestions();
+      const responseArr = await getGitResponse(data);
+      const page = generateHTML(data, responseArr);
+      writeToFile(filename, page);
+      convertToPDF(page);
+    } catch (error) {
+      console.log(chalk.inverse.yellow(`There was a problem ${error}`));
+    }
+  }
+  
+  init();
